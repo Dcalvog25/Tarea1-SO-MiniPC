9d47838b625df3efd00fc9e205fc059918405d6e
Dcalvog25 <d.calvo.1@estudiantec.cr>
2026-09-10 21:55:13 -0600

    Avance métodos en clase Memoria (base)

--- programa/T1SOMiniPC/src/main/java/com/minipc/t1sominipc/model/Memoria.java
+++ programa/T1SOMiniPC/src/main/java/com/minipc/t1sominipc/model/Memoria.java
@@ -10,7 +10,7 @@ package com.minipc.t1sominipc.model;
  */
 public class Memoria {
     
-    private String[] memoria;
+    private int[] memoria;
     private String[] memoriaLabels;
     private int tamañoTotal;
     private int finMemoriaKernel;
@@ -25,15 +25,70 @@ public class Memoria {
         this.tamañoTotal = tamañoTotal;
         this.finMemoriaKernel = tamañoKernel - 1;
         this.inicioMemoriaUsuario = tamañoKernel;
-        this.memoria = new String[tamañoTotal];
+        this.memoria = new int[tamañoTotal];
         this.memoriaLabels = new String[tamañoTotal];
         
         for (int i = 0; i < tamañoTotal; i++) {
-            memoria[i] = "00000000"; // 8 bits vacíos por defecto
+            memoria[i] = 0; 
             memoriaLabels[i] = "";
         }
         
     }
-    
-    
+
+    public int getTamañoTotal() {
+        return tamañoTotal;
+    }
+
+    public int getFinMemoriaKernel() {
+        return finMemoriaKernel;
+    }
+
+    public int getInicioMemoriaUsuario() {
+        return inicioMemoriaUsuario;
+    }
+
+    public int[] getMemoria() {
+        return memoria;
+    }
+
+    public String[] getMemoriaLabels() {
+        return memoriaLabels;
+    }
+
+    public boolean direccionValida(int direccion) {
+        if (direccion < 0 || direccion >= tamañoTotal) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean direccionValidaUsuario(int direccion) {
+        if (direccion < inicioMemoriaUsuario || direccion >= tamañoTotal) {
+            return false;
+        }
+        return true;
+    }
+
+    public void limpiarMemoria() {
+        for (int i = 0; i < tamañoTotal; i++) {
+            memoria[i] = 0; 
+            memoriaLabels[i] = "";
+        }
+    }
+
+    public int leer(int direccion) {
+        if (!direccionValida(direccion)) {
+            return 0; // Dirección inválida, devolver 0
+        }
+        return memoria[direccion];
+    }
+
+    public boolean escribir(int direccion, int valor, String label) {
+        if (!direccionValida(direccion)) {
+            return false; // Dirección inválida
+        }
+        memoria[direccion] = valor;
+        memoriaLabels[direccion] = label;
+        return true;
+    }
 }
